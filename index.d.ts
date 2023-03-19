37aece97b9e336e3f4d12e635bd385ce99bea7e2
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-03-19 23:47:41 +0700

    add only uppercase

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,14 @@
-export default function charRandom(count: number = 1, useNumber:boolean = false): string {
-    let textAll: string[] = []
-    if (useNumber) {
-   textAll  = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-  'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a',
-  'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-  't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1',
-  '2', '3', '4', '5', '6', '7', '8', '9'
-];
-} else {
-textAll = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-  'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a',
-  'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-];
+export default function charRandom(count: number = 1, useNumber:boolean = false, uppercase: boolean = false): string {
+  let textAll: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+  if(!uppercase) {
+
+    textAll.push(...[
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+  ]);
+  }
+    if (useNumber) {
+textAll.push(...[ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
 }
   let char: string = "";
   for (let i = 0; i < count; i++) {
--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-function charRandom(count = 1, useNumber = false){
-    let textAll = []
-    if (useNumber) {
-   textAll  = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-  'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a',
-  'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-  't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1',
-  '2', '3', '4', '5', '6', '7', '8', '9'
-];
-} else {
-textAll = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-  'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a',
-  'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-];
+function charRandom(count = 1, useNumber = false, uppercase = false) {
+  let textAll = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+  if(!uppercase) {
+
+    textAll.push(...[
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+  ]);
+  }
+    if (useNumber) {
+textAll.push(...[ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
 }
   let char = "";
   for (let i = 0; i < count; i++) {
@@ -27,5 +17,6 @@ textAll = [
   }
   return char;
 }
+
 
 module.exports =  charRandom;
